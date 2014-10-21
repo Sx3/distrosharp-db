630a5fbfebeb6d34c5493ccb34c12b5063c0715b
Mahesh Chathuranga <chathuranga@thesanmark.com>
2014-10-21 13:29:25 +0530

    Add Item Sales Details

--- main.sql
+++ main.sql
@@ -3,12 +3,18 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 21, 2014 at 10:12 AM
--- Server version: 5.5.40-0ubuntu0.14.04.1
+-- Generation Time: Oct 21, 2014 at 01:28 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `d-071-1-v2`
@@ -26,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `abilities` (
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=52 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=54 ;
 
 --
 -- Dumping data for table `abilities`
@@ -84,7 +90,8 @@ INSERT INTO `abilities` (`id`, `name`, `label`, `description`) VALUES
 (49, 'order_items', 'Order Items', 'Order items for selling invoice'),
 (50, 'view_profit_and_loss_report', 'Profit and Loss Report', 'Profit and Loss Report'),
 (51, 'view_incoming_cheques_report', 'Incoming Cheques Report', ''),
-(52, 'view_item_sales_summary_report', 'View Item Sales Summary Report', '');
+(52, 'view_item_sales_summary_report', 'View Item Sales Summary Report', ''),
+(53, 'view_item_sales_details_report', 'Item Sales Details', '');
 
 -- --------------------------------------------------------
 
@@ -136,3 +143,6 @@ INSERT INTO `system_settables` (`id`, `name`, `label`, `description`) VALUES
 (7, 'income_account', 'Income Account', ''),
 (8, 'expense_account', 'Epense Account', '');
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
