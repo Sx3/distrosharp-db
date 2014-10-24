7f10fdcf0441a8cc7b3d9ff94f9bc6224a0c9ff4
Randika Srimal <randika@thesanmark.com>
2014-10-24 10:03:48 +0530

    Add stock confirmation ability

--- main.sql
+++ main.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 22, 2014 at 01:18 PM
--- Server version: 5.5.40-0ubuntu0.14.04.1
+-- Generation Time: Oct 24, 2014 at 10:01 AM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `d-071-1-v2`
@@ -32,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `abilities` (
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=57 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=58 ;
 
 --
 -- Dumping data for table `abilities`
@@ -94,7 +88,8 @@ INSERT INTO `abilities` (`id`, `name`, `label`, `description`) VALUES
 (53, 'view_item_sales_details_report', 'Item Sales Details', ''),
 (54, 'view_rep_finance_report', 'View Rep Finance Report', ''),
 (55, 'view_item_return_report', 'Item Return Report', ''),
-(56, 'view_stock_vs_time_report', 'Stock vs Time', '');
+(56, 'view_stock_vs_time_report', 'Stock vs Time', ''),
+(57, 'confirm_stock', 'Confirm Stock', '');
 
 -- --------------------------------------------------------
 
@@ -146,6 +141,3 @@ INSERT INTO `system_settables` (`id`, `name`, `label`, `description`) VALUES
 (7, 'income_account', 'Income Account', ''),
 (8, 'expense_account', 'Epense Account', '');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
