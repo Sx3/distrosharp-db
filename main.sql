6c2f59739c4eb7616a1115e178dc255795fa853f
Supun Budhajeewa <budhajeewa@thesanmark.com>
2014-11-04 12:50:16 +0530

    Add "users" table.

--- main.sql
+++ main.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.2.6deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 31, 2014 at 09:06 AM
--- Server version: 5.5.38-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.4
+-- Generation Time: Nov 04, 2014 at 12:49 PM
+-- Server version: 5.5.40-0ubuntu1
+-- PHP Version: 5.5.12-2ubuntu4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,11 +21,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `abilities` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=60 ;
 
 --
@@ -92,11 +91,10 @@ INSERT INTO `abilities` (`id`, `name`, `label`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `stock_types` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
@@ -114,11 +112,10 @@ INSERT INTO `stock_types` (`id`, `name`, `label`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `system_settables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
 
 --
@@ -136,3 +133,70 @@ INSERT INTO `system_settables` (`id`, `name`, `label`, `description`) VALUES
 (8, 'expense_account', 'Epense Account', ''),
 (9, 'main_stock', 'main_stock', ''),
 (10, 'organization_name', 'Organization Name', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `remember_token`, `created_at`, `updated_at`) VALUES
+(999, 'sanmark', 'budhajeewa@thesanmark.com', '$2y$10$GsLTgJSY9u0p0vembfN2Y.IrpvRlIeXjH9PswmlTJL41KiWvMdV2K', 'Firstname', 'Lastname', 'P3BqQnVipDAs8wuwFPFSBAMpDG6SWU7DRj2VpznacbNDdynPZmSwVFbl2T3S', '2014-10-28 06:42:30', '2014-11-03 04:27:46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `abilities`
+--
+ALTER TABLE `abilities`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stock_types`
+--
+ALTER TABLE `stock_types`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settables`
+--
+ALTER TABLE `system_settables`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `abilities`
+--
+ALTER TABLE `abilities`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=60;
+--
+-- AUTO_INCREMENT for table `stock_types`
+--
+ALTER TABLE `stock_types`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `system_settables`
+--
+ALTER TABLE `system_settables`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
