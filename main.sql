2685c207edbc998fc9f1bbc02c5d6e17057158ed
Randika Srimal <randika@thesanmark.com>
2014-11-07 09:23:02 +0530

    Change 'confirm_bank_account_balance' to 'confirm_finance_account_balance'

--- main.sql
+++ main.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.6deb1
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 04, 2014 at 12:49 PM
--- Server version: 5.5.40-0ubuntu1
--- PHP Version: 5.5.12-2ubuntu4.1
+-- Generation Time: Nov 07, 2014 at 09:20 AM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,10 +21,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `abilities` (
-`id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=60 ;
 
 --
@@ -71,7 +72,7 @@ INSERT INTO `abilities` (`id`, `name`, `label`, `description`) VALUES
 (45, 'view_debtor_summary_report', 'Reports,View Debtor Summary Report', ''),
 (46, 'view_unload_comparison_report', 'Reports,View Unload Comparison Report', ''),
 (47, 'view_age_credit_report', 'Reports,View Age Credit Report', ''),
-(48, 'confirm_bank_account_balance', 'Finances,Accounts,Confirm', ''),
+(48, 'confirm_finance_account_balance', 'Finances,Accounts,Confirm', ''),
 (49, 'order_items', 'Entities,Items,Order', 'Order items for selling invoice'),
 (50, 'view_profit_and_loss_report', 'Reports,View Profit and Loss Report', 'Profit and Loss Report'),
 (51, 'view_incoming_cheques_report', 'Reports,View Incoming Cheques Report', ''),
@@ -91,10 +92,11 @@ INSERT INTO `abilities` (`id`, `name`, `label`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `stock_types` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
@@ -112,10 +114,11 @@ INSERT INTO `stock_types` (`id`, `name`, `label`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `system_settables` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
 
 --
@@ -158,45 +161,3 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `remember_token`, `created_at`, `updated_at`) VALUES
 (999, 'sanmark', 'budhajeewa@thesanmark.com', '$2y$10$GsLTgJSY9u0p0vembfN2Y.IrpvRlIeXjH9PswmlTJL41KiWvMdV2K', 'Firstname', 'Lastname', 'P3BqQnVipDAs8wuwFPFSBAMpDG6SWU7DRj2VpznacbNDdynPZmSwVFbl2T3S', '2014-10-28 06:42:30', '2014-11-03 04:27:46');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `abilities`
---
-ALTER TABLE `abilities`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `stock_types`
---
-ALTER TABLE `stock_types`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `system_settables`
---
-ALTER TABLE `system_settables`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `abilities`
---
-ALTER TABLE `abilities`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=60;
---
--- AUTO_INCREMENT for table `stock_types`
---
-ALTER TABLE `stock_types`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `system_settables`
---
-ALTER TABLE `system_settables`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
