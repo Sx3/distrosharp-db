45d5081694c99efd28882c07049dbd4c57ee4786
Supun Budhajeewa <budhajeewa@thesanmark.com>
2014-12-02 15:04:37 +0530

    Remove permissions
     - view_stocks
     - edit_stock

--- main.sql
+++ main.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.2.6deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 26, 2014 at 03:02 PM
--- Server version: 5.5.40-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- Generation Time: Dec 02, 2014 at 03:02 PM
+-- Server version: 5.5.40-0ubuntu1
+-- PHP Version: 5.5.12-2ubuntu4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,11 +21,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `abilities` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=64 ;
 
 --
@@ -53,8 +52,6 @@ INSERT INTO `abilities` (`id`, `name`, `label`, `description`) VALUES
 (25, 'edit_purchase', 'Processes,Purchases,Edit', ''),
 (26, 'view_transfers', 'Processes,Transfers,View', '??'),
 (27, 'add_transfer', 'Processes,Transfers,Add', ''),
-(29, 'view_stocks', 'Stocks,View', ''),
-(30, 'edit_stock', 'Stocks,Edit', ''),
 (31, 'view_sales', 'Processes,Sales,View', ''),
 (32, 'add_sale', 'Processes,Sales,Add', ''),
 (33, 'edit_sale', 'Processes,Sales,Edit', ''),
@@ -87,11 +84,10 @@ INSERT INTO `abilities` (`id`, `name`, `label`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `stock_types` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
@@ -109,11 +105,10 @@ INSERT INTO `stock_types` (`id`, `name`, `label`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `system_settables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `description` mediumtext COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
 
 --
@@ -156,3 +151,45 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `remember_token`, `created_at`, `updated_at`) VALUES
 (999, 'sanmark', 'budhajeewa@thesanmark.com', '$2y$10$GsLTgJSY9u0p0vembfN2Y.IrpvRlIeXjH9PswmlTJL41KiWvMdV2K', 'Firstname', 'Lastname', 'P3BqQnVipDAs8wuwFPFSBAMpDG6SWU7DRj2VpznacbNDdynPZmSwVFbl2T3S', '2014-10-28 06:42:30', '2014-11-03 04:27:46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `abilities`
+--
+ALTER TABLE `abilities`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stock_types`
+--
+ALTER TABLE `stock_types`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settables`
+--
+ALTER TABLE `system_settables`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `abilities`
+--
+ALTER TABLE `abilities`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=64;
+--
+-- AUTO_INCREMENT for table `stock_types`
+--
+ALTER TABLE `stock_types`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `system_settables`
+--
+ALTER TABLE `system_settables`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
